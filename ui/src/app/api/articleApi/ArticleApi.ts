89bbc22f6d174acc400955ba3c09297c72b87795
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2024-06-02 19:02:43 -0500

    153: Starting code for getting API data

---
+++ ui/src/app/api/articleApi/ArticleApi.ts
@@ -0,0 +1,45 @@
+import { useGetApi } from "@/app/hooks/useGetApi";
+import { Article } from "@pages/home/HomeTypes";
+
+export const ARTICLE_API_URL = "/api/article";
+
+export const useGetArticles = () => {
+  const { status, data, error } = useGetApi<Article[]>({
+    url: ARTICLE_API_URL,
+    config: {},
+  });
+
+  return {
+    articleStatus: status,
+    articleData: data,
+    articleError: error,
+  };
+};
+
+export const useGetArticleById = (articleId: number) => {
+  const { status, data, error } = useGetApi<Article>({
+    url: `${ARTICLE_API_URL}/${articleId}`,
+    config: {},
+  });
+
+  return {
+    articleStatus: status,
+    articleData: data,
+    articleError: error,
+  };
+};
+
+export const useGetCriticalArticles = () => {
+  const { status, data, error } = useGetApi<Article[]>({
+    url: ARTICLE_API_URL,
+    config: {
+      critical: true,
+    },
+  });
+
+  return {
+    articleStatus: status,
+    articleData: data,
+    articleError: error,
+  };
+};
